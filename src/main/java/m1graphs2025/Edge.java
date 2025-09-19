3ffcef40db86ebd6f709d1415c23609e1b20eb5d
rtatin <raphael.tatin@edu.univ-fcomte.fr>
2025-09-19 22:59:01 +0200

    Edge and node modifications

--- src/main/java/m1graphs2025/Edge.java
+++ src/main/java/m1graphs2025/Edge.java
@@ -1,42 +1,133 @@
 package m1graphs2025;
 
+import java.util.Objects;
+
+/**
+ * This class represents a directed edge between two Node objects in a graph structure
+ * 
+ * An edge connects a source node ("from") to a target node ("to") and it may optionally have an associated weight
+ * 
+ * @see Node
+ * @see Graph
+ */
 public class Edge implements Comparable<Edge>{
-  Node from;
-  Node to;
-  int weight;
+  private Node from;
+  private Node to;
+  private Integer weight;
 
-  public Edge() {
+  /**
+   * Constructs a directed edge between two nodes with an optional weight
+   *
+   * @param from   the source node (must not be null)
+   * @param to     the target node (must not be null)
+   * @param weight the wieght of the edge, or null for unweighted
+   * @throws IllegalArgumentException if from or to is null
+   */
+  public Edge(Node from, Node to, Integer weight) {
+    if (from == null || to == null) {
+      throw new IllegalArgumentException("From and to cannot be null");
+    }
 
+    this.from = from;
+    this.to = to;
+    this.weight = weight;
   }
 
+  /**
+   * Constructs an unweighted directed edge between two nodes
+   *
+   * @param from the sorce node (must not be null)
+   * @param to the target node (must not be null)
+   * @throws IllegalArgumentException if from or to is null
+   */
+  public Edge(Node from, Node to) {
+    this(from, to, null);
+  }
+
+  /**
+   * Constructs a directed edge from node IDs with an optional weight
+   *
+   * @param fromId the ID of the source node
+   * @param toId   the ID of the target node
+   * @param weight thie weight of the edge, or null for unweighted
+   * @throws IllegalArgumentException if fromId or toId <= 0
+   */
+  public Edge(int fromId, int toId, Integer weight) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  /**
+   * Constructs an unweighted directed edge from node IDs
+   *
+   * @param fromId the ID of the source node
+   * @param toId   the ID of the target node
+   * @throws IllegalArgumentException of fromId or toId <= 0
+   */
+  public Edge(int fromId, int toId) {
+    this(fromId, toId, null);
+  }
+
+  /**
+   * @return the source node of this edge
+   */
   public Node from() {
     return this.from;
   }
 
+  /**
+   * @return the target node of this edge
+   */
   public Node to() {
     return this.to;
   }
 
+  /**
+   * Returns a new edge with the same weight but reversed direction
+   * 
+   * @return a symmetric edge (from -> to becomes to -> from)
+   */
   public Edge getSymmetric() {
-    return new Edge(to, weight, from);
+    return new Edge(to, from, weight);
   }
 
+  /**
+   * Checks whether this edge is a self-loop
+   *
+   * @return true if this edge is a self-loop; false otherwier
+   */
   public boolean isSelfLoop() {
     return from == to;
   }
 
+  /**
+   * TODO
+   */
   public boolean isMultiEdge() {
-    
+    throw new UnsupportedOperationException("Not implemented");
   }
 
+  /**
+   * @return true if the edge has a weight; false if it is unweighted
+   */
   public boolean isWeighted() {
-    return weight == null;
+    return weight != null;
   }
 
+  /**
+   * @return the weight of this edge, or null if unweighted
+   */
   public Integer getWeight() {
     return this.weight;
   }
 
+  /**
+   * Compares this edge to another object for equality
+   *
+   * Two edges are considerd equal if they have the same source node, targe node, and weight
+   *
+   * @param obj the object to comapre
+   * @return true if obj is an Edge with the same properties
+   */
   @Override
   public boolean equals(Object obj) {
     if (this == obj) {
@@ -48,25 +139,35 @@ public class Edge implements Comparable<Edge>{
     }
 
     Edge other = (Edge)obj;
-
-    return (this.from == other.from && this.to == other.to && this.weight == other.weight);
+    return (this.from.equals(other.from) && this.to.equals(other.to) && this.weight.equals(other.weight));
   }
 
+  /**
+   * @return a hash code consistent with Object
+   */
   @Override
   public int hashCode() {
-    return this.;
+    return Objects.hash(this.from, this.to, this.weight);
   }
 
+  /**
+   * Compares this edge to another edge for ordering
+   *
+   * The comparison is performed in the followind order : source, target, weight
+   *
+   * @param o the other edge
+   * @return a negative integer, zero, or a positive integer as ths edge is less than, equal to, or greated than the specified edge
+   */
   @Override
-  public int compareTo(Node o) {
+  public int compareTo(Edge o) {
     if (this.from != o.from) {
-      return Node.compareTo(this.from, o.from);
+      return this.from.compareTo(o.from);
     }
 
     if (this.to != o.to) {
-      return Node.compareTo(this.to, o.to);
+      return this.to.compareTo(o.to);
     }
 
-    return Integer.compareTo(this.weight, o.weight);
+    return this.weight.compareTo(o.weight);
   }
 }
--- src/main/java/m1graphs2025/Node.java
+++ src/main/java/m1graphs2025/Node.java
@@ -1,76 +1,226 @@
 package m1graphs2025;
 
-public class Node implements Comparable<Node>{
-  final int id;
-  Graph graphHolder;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.function.Function;
 
+/**
+ * This class represents a node (vertex) in a graph
+ *
+ * A node is identified by a unique integer ID and may have an associated name
+ * 
+ * @see Graph
+ * @see Edge
+ */
+public class Node implements Comparable<Node>{
+  private final int id;
+  private final String name;
+  private Graph graphHolder;
+
+  /**
+   * Creates a new node
+   *
+   * @param id          the unique identifier of the node, must be greater than 0
+   * @param name        the display name of the node (may be empty, but not null)
+   * @param graphHolder the graph that owns this node
+   * @throws IllegalArgumentException if id <= 0
+   */
   public Node(int id, String name, Graph graphHolder) {
+    if (this.id < 1) {
+      throw new IllegalArgumentException("Node id must be higher than 0");
+    }
     this.id = id;
     this.name = name;
     this.graphHolder = graphHolder;
   }
 
-
+  /**
+   *  Creates a new node with an empty name
+   *
+   * @param id          the unique identifier of the node, must be greate than 0
+   * @param graphHolder the graph that owns this node
+   * @throws IllegalArgumentException if id <= 0
+   */
   public Node(int id, Graph graphHolder) {
     this(id, "", graphHolder);
   }
 
-  public Node() {
-
-  }
-
+  /**
+   * @retrun the node's unique identifier
+   */
   public int getId() {
     return this.id;
   }
-
+  
+  /**
+   * @return the graph to which this node belongs
+   */
   public Graph getGraph() {
-    return null;
+    return graphHolder;
   }
 
+  /**
+   * @return the display name of this node (may be empty but not null)
+   */
   public String getName() {
     return this.name;
   }
-
+  
+  /**
+   * Gets all unique successors of this node (nodes reachable via outgoind edges)
+   *
+   * @return a list of distinct successor nodes
+   */
   public List<Node> getSuccessors() {
-    return new ArrayList<Node>();
+    Set<Node> succ = new HashSet<Node>();
+    getOutEdges().foreach(e -> succ.add(e.to()));
+    return new ArrayList<Node>(succ);
   }
 
+  /**
+   * Gets all successors of this node, keeping duplicates
+   * (i.e., one entry per outgoind edge)
+   *
+   * @return a list of successor nodes, possibly with duplicates
+   */
   public List<Node> getSuccessorsMulti() {
-    return new ArrayList<Node>();
+    List<Node> succ = new ArrayList<Node>();
+    getOutEdges().foreach(e -> succ.add(e.to()));
+    return succ;
   }
 
-  public bool adjacent(Node u) {
-    return true;
+  /**
+   * Checks whether this node is adjacent to another node
+   *
+   * @param u the other node
+   * @return true if an edge from this node to u
+   * @throws NullPointerException if u is null
+   */
+  public boolean adjacent(Node u) {
+    if (u == null) {
+      throw new NullPointerException("Node must be initialize");
+    }
+    return adjacent(u.getId());
   }
 
+  /**
+   * Checks whether this node is adjacent to a node with the given ID
+   *
+   * @param nodeId the identifier of the other node
+   * @return true if an edge from this node to the node with nodeId
+   * @throws IllegalArgumentException if nodeId <= 0
+   */
+  public boolean adjacent(int nodeId) {
+    if (nodeId <= 0) {
+      throw new IllegalArgumentException("Node ID must be higher than 0");
+    }
+
+    for (Edge e : getOutEdges()) {
+      if (e.to().getId() == nodeId) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * @return the number of incoming edges of this node
+   */
   public int inDegree() {
-    return -1;
+    return getInEdges().size();
   }
 
+  /**
+   * @return the number of outgoing edges of this node
+   */
   public int outDegree() {
-    return -1;
+    return getOutEdges().size();
   }
 
+  /**
+   *  @return the total degree (sum of in-degree and out-degree) of this node
+   */
   public int degree() {
-    return -1;
+    return inDegree() + outDegree();
   }
 
+  /**
+   * @return a list of outgoing edges from this node
+   */
   public List<Edge> getOutEdges() {
-    return new ArrayList<Edge>();
+    return getEdges(Edge::to);
   }
 
+  /**
+   * @return a list of incoming edges to this node 
+   */
   public List<Edge> getInEdges() {
-    return new ArrayList<Edge>();
+    return getEdges(Edge::from);
   }
 
+  /**
+   * @return a list of all incident edges (incoming + outgoing)
+   */
   public List<Edge> getIncidentEdges() {
-    return new ArrayList<Edge>();
+    return getOutEdges().addAll(getInEdges());
   }
 
+  /**
+   * Gets all edges from this node to another node
+   *
+   * @param u the target node
+   * @return a list of edges from this node to u
+   */
   public List<Edge> getEdgesTo(Node u) {
-    return new ArrayList<Edge>();
+    return getEdgesTo(u.getId());
   }
 
+  /**
+   * Gets all edges from this node to a node with a given ID
+   *
+   * @param nodeId the identifier of the target node
+   * @return a list of edges from this node to the node with nodeId
+   * @throws IllegalArgumentException if nodeId <= 0
+   */
+  public List<Edge> getEdgesTo(int nodeId) {
+    if (nodeId <= 0) {
+      throw new IllegalArgumentException("Node ID must be higher than 0");
+    }
+    List<Edge> result = new ArrayList<Edge>();
+    for (Edge e : getOutEdges()) {
+      if (e.to().getId() == nodeId) {
+        result.add(e);
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Helper method to get edges associated with this node, depending on a selector function
+   *
+   * @param selector a function mapping an edge to one of its endpoints
+   * @return a list of edges where selector.apply(edge) == this
+   */
+  private List<Edge> getEdges(Function<Edge, Node> selector) {
+    List<Edge> result = new ArrayList<Edge>();
+    for (Edge e : graphHolder.getEdges()) {
+      if (selector.apply(e).equals(this)) {
+        result.add(e);
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Comapres this node to another object for equality
+   *
+   * Nodes are considered equal if they have the same ID and the same name
+   *
+   * @param obj the object to compare
+   * @return true if obj is a Node with the same ID and name
+   */
   @Override
   public boolean equals(Object obj) {
     if (this == obj) {
@@ -82,15 +232,23 @@ public class Node implements Comparable<Node>{
     }
 
     Node other = (Node)obj;
-
-    return (this.id == other.id && this.name == other.name);
+    return (this.id.equals(other.id) && this.name.equals(other.name));
   }
 
+/**
+ * @return a hash code consistent with Object
+ */
   @Override
   public int hashCode() {
-    return this.id;
+    return Object.hash(this.id);
   }
 
+ /**
+  * Compares this node to another node by ID
+  *
+  * @param o the other node
+  * @return a negative integer, zero, or a positive integer if this node's ID is less than, equal to, or greater than o's ID
+  */
   @Override
   public int compareTo(Node o) {
     return this.id - o.id;
