1d7bd1991f45b1e13fe583d14aab70958abea89e
nfaivr10 <noam.faivre@edu.univ-fcomte.fr>
2025-11-12 11:37:31 +0100

    add get and set for FlowEdge

--- src/main/java/m1maxflow2025/FlowEdge.java
+++ src/main/java/m1maxflow2025/FlowEdge.java
@@ -4,13 +4,20 @@ import m1graphs2025.Graph;
 import m1graphs2025.Node;
 
 public class FlowEdge extends Edge{
-    Integer flow;
+    private Integer flow;
 
     public FlowEdge(Node from, Node to, Graph graphHolder, Integer weight, Integer flow) {
         super(from, to, graphHolder, weight);
+        this.flow = flow;
     }
 
+    public Integer getFlow(){
+        return this.flow;
+    }
 
+    public void setFlow(Integer newflow){
+        this.flow = newflow;
+    }
 
 
 }
