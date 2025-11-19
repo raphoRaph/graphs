8ad03a7d06a61d02f29cd77bdb106798f628b4b1
nfaivr10 <noam.faivre@edu.univ-fcomte.fr>
2025-11-19 11:52:52 +0100

    update toDotString for FlowNetwork by adding the value

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # graphs
+
+For the second project we had to modify the toString method in Node. Back then the to String method return a String representing the number of the node. Now it return it's name if it have one, and it's number if not.
+
+
--- src/main/java/m1maxflow2025/FlowNetwork.java
+++ src/main/java/m1maxflow2025/FlowNetwork.java
@@ -2,6 +2,7 @@ package m1maxflow2025;
 
 import java.util.ArrayList;
 
+import m1graphs2025.Edge;
 import m1graphs2025.Graph;
 import m1graphs2025.Node;
 
@@ -24,19 +25,32 @@ public class FlowNetwork extends Graph {
 		adjEdList.get(from).add(new FlowEdge(from, to, this, weight, flow));
 	}
 
+	private int flowValue(){
+		int res = 0;
+		for (Edge edge: getOutEdges(smallestNodeId())) {
+			FlowEdge flow = (FlowEdge) edge;
+			res += flow.getFlow();	
+		}
+		return res;
+	}
+
+
 	@Override
 	public String toDotString() {
 		StringBuilder sb = new StringBuilder();
 
 		sb.append("digraph {\n").append("\trankdir=LR\n");
+		sb.append("label =\"Value: ").append(flowValue()).append("\n");
 
-		ArrayList<FlowEdge> lst = (ArrayList) getAllEdges();
+		ArrayList<Edge> lst = (ArrayList) getAllEdges();
+
 		lst.sort(null);
-		for (FlowEdge edge : lst) {
-			sb.append("\t").append(edge.from()).append(" ").append("->").append(" ").append(edge.to());
-			if (edge.isWeighted()) {
-				String div = edge.getFlow() + "/" + edge.getWeight();
-				sb.append(" [label=").append(edge.getFlow() != 0 ? div : edge.getWeight()).append(", len=").append(edge.getWeight()).append("]");
+		for (Edge edge : lst) {
+			FlowEdge flow = (FlowEdge) edge;
+			sb.append("\t").append(flow.from()).append(" ").append("->").append(" ").append(flow.to());
+			if (flow.isWeighted()) {
+				String div = flow.getFlow() + "/" + flow.getWeight();
+				sb.append(" [label=").append(flow.getFlow() != 0 ? div : flow.getWeight()).append(", len=").append(flow.getWeight()).append("]");
 			}
 			sb.append("\n");
 		}
