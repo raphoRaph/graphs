d6e221fd1708e4b357af5a835f36e844d50bae17
nfaivr10 <noam.faivre@edu.univ-fcomte.fr>
2025-11-19 11:05:58 +0100

    add toDotString for FlowNetwork

--- src/main/java/m1maxflow2025/FlowNetwork.java
+++ src/main/java/m1maxflow2025/FlowNetwork.java
@@ -1,5 +1,7 @@
 package m1maxflow2025;
 
+import java.util.ArrayList;
+
 import m1graphs2025.Graph;
 import m1graphs2025.Node;
 
@@ -23,8 +25,28 @@ public class FlowNetwork extends Graph {
 	}
 
 	@Override
-	public void toDotFile(String fileName) {
-		// TODO Auto-generated method stub
-		super.toDotFile(fileName);
+	public String toDotString() {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("digraph {\n").append("\trankdir=LR\n");
+
+		ArrayList<FlowEdge> lst = (ArrayList) getAllEdges();
+		lst.sort(null);
+		for (FlowEdge edge : lst) {
+			sb.append("\t").append(edge.from()).append(" ").append("->").append(" ").append(edge.to());
+			if (edge.isWeighted()) {
+				String div = edge.getWeight() + "/" + edge.getFlow();
+				sb.append(" [label=").append(edge.getFlow() != 0 ? div : edge.getWeight()).append(", len=").append(edge.getWeight()).append("]");
+			}
+			sb.append("\n");
+		}
+    
+		for (Node node : getAllNodes()) {
+			if (getInEdges(node).isEmpty() && getOutEdges(node).isEmpty()) {
+				sb.append("\t").append(node).append("\n");
+			}
+		}
+		sb.append("}\n");
+		return sb.toString();
 	}
 }
