32808923628b7eb986b3c0206f08f61f8f566255
nfaivr10 <noam.faivre@edu.univ-fcomte.fr>
2025-11-19 12:22:36 +0100

    add toDotFile for RedidualGraph

--- src/main/java/m1maxflow2025/ResidualGraph.java
+++ src/main/java/m1maxflow2025/ResidualGraph.java
@@ -1,13 +1,14 @@
 package m1maxflow2025;
 
-import m1graphs2025.Graph;
-
+import java.util.ArrayList;
 import java.util.List;
 
 import m1graphs2025.Edge;
+import m1graphs2025.Graph;
+import m1graphs2025.Node;
 
 public class ResidualGraph extends Graph {
-	private List<Integer> lastPath;
+	private List<Node> lastPath;
 	Integer residualCapacity;
 
 	public ResidualGraph() {
@@ -37,12 +38,12 @@ public class ResidualGraph extends Graph {
 		return residualGraph;
 	}
 
-	public int bottleneckOf(List<Integer> path) {
+	public int bottleneckOf(List<Node> path) {
 		int b = Integer.MAX_VALUE;
 
 		for (int i = 0; i < path.size() - 1; i++) {
-			int u = path.get(i);
-			int v = path.get(i + 1);
+			Node u = path.get(i);
+			Node v = path.get(i + 1);
 
 			int cap = (getResidualCapacity(u, v));
 
@@ -53,18 +54,57 @@ public class ResidualGraph extends Graph {
 		return b;
 	}
 
-	private int getResidualCapacity(int u, int v) {
+	private int getResidualCapacity(Node u, Node v) {
 		for (Edge e : getOutEdges(u)) {
-			if (e.to().getId() == v) {
+			if (e.to() == v) {
 				return e.getWeight();
 			}
 		}
 		return 0;
 	}
 
+	private String pathString(){
+		String path = "Augmenting path: [";
+		for (int i = 0; i < lastPath.size(); i++){
+			path += lastPath.get(i).toString();
+			if(i != lastPath.size() -1){
+				path += ", ";
+			}
+		}
+		path += "].\n";
+		return path;
+	}
+
 	@Override
 	public String toDotString() {
-		// TODO Auto-generated method stub
-		return super.toDotString();
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("digraph {\n").append("\trankdir=LR\n");
+		String path = pathString();
+		String resiCap = "Residual capacity: " + residualCapacity;
+		sb.append("label = residual graph.\n").append(path).append(resiCap).append("\n");
+
+		ArrayList<Edge> lst = (ArrayList) getAllEdges();
+
+		lst.sort(null);
+		for (Edge edge : lst) {
+			FlowEdge flow = (FlowEdge) edge;
+			String bigArrow = "";
+			if (lastPath.contains(edge.from()) && lastPath.contains(edge.to())){
+				bigArrow = ", penwidth=3, color=\"blue\"";
+			}
+			sb.append("\t").append(flow.from()).append(" ").append("->").append(" ").append(flow.to());
+			String label = (flow.getFlow() != 0) ? (flow.getFlow() + "/" + flow.getWeight()) : "" + flow.getWeight();
+			sb.append(" [label=").append(label).append(", len=").append(flow.getWeight()).append(bigArrow).append("]");
+			sb.append("\n");
+		}
+    
+		for (Node node : getAllNodes()) {
+			if (getInEdges(node).isEmpty() && getOutEdges(node).isEmpty()) {
+				sb.append("\t").append(node).append("\n");
+			}
+		}
+		sb.append("}\n");
+		return sb.toString();
 	}
 }
